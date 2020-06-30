dd632011450bcf3373af0d5ac97c49600df06364
ahmad <akhn358@gmail.com>
2020-06-30 15:25:14 +0500

    Commiting files after completing post view

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.dashboard, name="dashboard"),
     path('posts', views.posts, name="posts"),
     re_path(r'^post/(?P<id>[0-9_]+)/$', views.post_view, name="postview"),
+    path('post/analyze_post', views.analyze_post, name="analyzepost"),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
 from .sentiment_analysis import SentimentAnalysis
@@ -29,30 +30,7 @@ def posts(request):
 @login_required
 def post_view(request, id):
     post = data.fetch_one_post(id)
-    # p_count = 0
-    # n_count = 0
-
-    # comments = []
-  
-    # analysis.create_model()
-    # accuracy = str(round(analysis.get_accuracy() * 100)) + " %"
-    # prediction = []
-
-    # if 'comments' in post.keys():
-    #     for comment in post['comments']['data']:
-    #         comments.append(comment['message'])
-
-    #     if len(comments) == 1:
-    #         prediction = [[comments[0], analysis.predict_one_review(comments[0])]]
-
-    #     else:
-    #         prediction = analysis.predict_many_reviews(comments)
-
-    #         for s_count in prediction:
-    #             if s_count[1] == "Positive":
-    #                 p_count += 1
-    #             else:
-    #                 n_count += 1
+   
         
 
     context = {
@@ -65,7 +43,37 @@ def post_view(request, id):
     
     return render(request, 'dashboard/postview.html', context)
 
-
-
+def analyze_post(request):
+    if request.is_ajax and request.method == "GET":
+        comments = request.GET.getlist('comments[]')
+        p_count = 0
+        n_count = 0
     
+        analysis.create_model()
+        accuracy = str(round(analysis.get_accuracy() * 100)) + " %"
+        prediction = []
+
+        
+        if len(comments) == 1:
+            prediction = [[comments[0], analysis.predict_one_review(comments[0])]]
+
+        else:
+            prediction = analysis.predict_many_reviews(comments)
+
+            for s_count in prediction:
+                if s_count[1] == "Positive":
+                    p_count += 1
+                else:
+                    n_count += 1
+        return JsonResponse({
+            "p_count": p_count,
+            "n_count": n_count,
+            "accuracy": accuracy,
+            "prediction": prediction
+            }, status=200)
+    else:
+        return JsonResponse({"error": "An error occurred"}, status=400)
+    
+
+
     
