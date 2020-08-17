5710c4a01f5e63adfcaa75afae1e9fb222d25f9e
ahmad <akhn358@gmail.com>
2020-08-17 21:00:07 +0500

    Final

--- dashboard/facebook_credentials.py
+++ dashboard/facebook_credentials.py
@@ -1,6 +1,7 @@
 class Credentials:
     def __init__(self):
-        self.TOKEN = "EAAHtsVeAtZCkBAFKn1AWBOaoRK5k1XFf8JJa1BFe11idx5AuI7gg8GydF0ES7a0W3OyQ8ZAku93xBQkrYtFCJf4F0LXxRxkqfUsCbZBOHB4UNm9nINKdS3TSwQBh8htGLVypZAb08JDcx02nlhhaoT0Y9BMbL1j1Hg05GXZCbzgZDZD"
+        # Your Facebook access token
+        self.TOKEN = ""
     
     def get_token(self):
         return self.TOKEN
